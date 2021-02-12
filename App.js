86daa0c9126522d0d533a335772ecc0ec84f8e28
Paramvir Singh <ps17cw@brocku.ca>
2021-02-11 19:03:32 -0500

    skeleton of menu

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { version } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+import Header from './Header';
+import Dish from './Dish';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+	const foodItems = [
+		{name: "Ice-Cream", info: "dfdsf sdf sdf dsf dsf ddf dsf dsf dfd df dsjfdskjfd vjlv bj ksdjfklds fdsf ", cost: "$5.59"},
+		{name: "Biryani", info: "dfdsf sdf sdf dsf dsf ddf dsf dsf dfd df dsjfdskjfd vjlv bj ksdjfklds fdsf ", cost: "$5.59"},
+		{name: "NY Pizza", info: "dfdsf sdf sdf dsf dsf ddf dsf dsf dfd df dsjfdskjfd vjlv bj ksdjfklds fdsf ", cost: "$5.59"},
+		{name: "Burger", info: "dfdsf sdf sdf dsf dsf ddf dsf dsf dfd df dsjfdskjfd vjlv bj ksdjfklds fdsf ", cost: "$5.59"},
+		{name: "Pop", info: "dfdsf sdf sdf dsf dsf ddf dsf dsf dfd df dsjfdskjfd vjlv bj ksdjfklds fdsf ", cost: "$5.59"},
+		{name: "Chicken Tenders", info: "dfdsf sdf sdf dsf dsf ddf dsf dsf dfd df dsjfdskjfd vjlv bj ksdjfklds fdsf ", cost: "$5.59"},
+		{name: "Water", info: "dfdsf sdf sdf dsf dsf ddf dsf dsf dfd df dsjfdskjfd vjlv bj ksdjfklds fdsf ", cost: "$5.59"},
+	];
+
+
+	return (
+		<View style={styles.main}>
+
+			<Header title="MENU" />
+
+			<ScrollView>
+				{foodItems.map(entry => (
+					<View style={styles.dishContainer} key={entry}>
+						<Dish name={entry.name} info={entry.info} cost={entry.cost}/>
+					</View>
+				))}
+			</ScrollView>
+
+			<Header title="TAP DISH TO VIEW 3D MODEL" />
+
+
+					
+
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+	main: {
+		flex: 1,
+		justifyContent: 'space-between',	
+	},
+
+
+	dishContainer:{
+		alignItems: 'center',
+	},
+
+	dish:{
+		fontSize: 56,
+	}
+
+
+
+})
---
+++ Dish.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const Dish = props => {
+    return (
+        <View style={styles.dishView}>
+
+            <View style={styles.nameAndCost}>
+                <Text style={{fontSize:30, fontWeight: 'bold', color: 'black',}} >{props.name}</Text>
+                <Text style={{fontSize:30, color: 'black',}} >{props.cost}</Text> 
+            </View>
+
+            <View>
+            <Text style={{fontSize:15, color: 'black',}}>{props.info}</Text>
+               
+            </View>
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+    dishView: {
+        flexDirection: 'column',
+        width: '100%',
+        height: 100,
+        alignItems: 'center',
+        borderWidth: 1
+        
+    },
+
+    nameAndCost: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+    },
+
+
+});
+
+
+export default Dish;
