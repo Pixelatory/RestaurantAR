df9f0c30622b068340b5d47b2ebb751ccadac192
fahad-ansar <fa16np@brocku.ca>
2021-02-14 07:29:20 -0800

    external stylesheet styles added

--- externalss.js
+++ externalss.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-//two main colors of the app
+//colors of the app
 var { Primary } = '#fff'; 
 var {Secondary } = '#ggg';
+var { BtnTextColor } = '#000';
+var { TextColorNormal } = '#111';
+var { TextColorSpecial } = '#111';
+
+
 
 const external = StyleSheet.create({
-    sample: {
+
+    styles: {
       flex: 1,
       backgroundColor: Primary,
-      color: Secondary,
+      color: TextColorNormal,
       alignItems: 'center',
       justifyContent: 'center',
     },  
+
+    buttonStyle: {
+      backgroundColor: Secondary,
+      color: BtnTextColor,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius:10,
+    },
+
+    headings:{
+      fontFamily: "sans-serif",
+      color: TextColorNormal,
+      fontsize: 22,
+    },
+
+    subHeadings:{
+      fontFamily: "sans-serif",
+      color: TextColorNormal,
+      fontsize: 18,
+    },
+
+    texts:{
+      fontFamily: "sans-serif",
+      color: TextColorNormal,
+      fontsize: 10,
+    }
+
 });
 
   
